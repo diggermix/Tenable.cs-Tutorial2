3d419363182d50bf45e11db55275776f838d3469
diggermix <mike@fikuart.com>
2023-08-17 14:37:55 +0100

    Corrections

--- tcs-workshop.tf
+++ tcs-workshop.tf
@@ -15,13 +15,11 @@ resource "aws_s3_bucket" "mf_ps_workshop_s3_001" {
   bucket = "mf-ps-workshop-s3-001-20230817"
 
   tags = {
-    "Name"      = "jdlab-ps-workshop-s3-001-20230816"
     "Terraform" = "true"
   }
 }
 
 resource "aws_s3_bucket_versioning" "mf-ps-workshop-s3-001-versioning" {
-  bucket = aws_s3_bucket.MF_ps_workshop_s3_001.id
   versioning_configuration {
     status = "Disabled"
   }
