4eddff4ed5624fc37cd9db77fada4a38f349dcb2
diggermix <mike@fikuart.com>
2023-08-17 16:51:22 +0100

    Corrections to S3 naming

--- tcs-workshop.tf
+++ tcs-workshop.tf
@@ -15,10 +15,13 @@ resource "aws_s3_bucket" "mf_ps_workshop_s3_001" {
   bucket = "mf-ps-workshop-s3-001-20230817"
 
   tags = {
+    "Name"      = "mf-ps-workshop-s3-001-20230817"
     "Terraform" = "true"
   }
 }
 
+resource "aws_s3_bucket_versioning" "mf_ps_workshop_s3_001_versioning" {
+  bucket = aws_s3_bucket.mf_ps_workshop_s3_001.id
   versioning_configuration {
     status = "Disabled"
   }
